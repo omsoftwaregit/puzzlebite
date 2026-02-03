baab2f938833f695832e2c977bc39e850cafb0b7
omsoftwaregit <info@omscorps.com>
2026-02-03 16:05:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,21 @@
 //   base: './', // important for S3 deployment
 // })
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/puzzlebite/',
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/puzzlebite/',
+  base: '/puzzlebite/', // this tells the browser that all assets are under /puzzlebite/
+  build: {
+    outDir: 'dist/puzzlebite', // 🔹 tell Vite to build directly into the 'puzzlebite' folder
+  },
 })
-
