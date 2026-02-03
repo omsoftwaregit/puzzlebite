0b565669c20fd68cd7e0578a22c0d827809362bc
omsoftwaregit <info@omscorps.com>
2026-02-03 15:33:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/puzzlebite/',   // IMPORTANT: folder name in S3
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/puzzlebite/',
 })
+
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
